248f59fa09de9bb7fce8ba1c8a49fa7fb65db863
AMAN7040 <amansingh79620@gmail.com>
2024-10-16 02:08:59 +0530

    ejs and static route setup

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const urlRoute = require('./routes/urlRoutes')
-const {connectToDb} = require('./db')
+const urlRoute = require("./routes/urlRoutes");
+const staticRoute = require("./routes/staticRoute");
+const path = require("path");
+const { connectToDb } = require("./db");
 const app = express();
 const PORT = 3000;
 
-connectToDb("mongodb://127.0.0.1:27017/short-url")
-.then(()=> console.log('database connected'));
+connectToDb("mongodb://127.0.0.1:27017/short-url").then(() =>
+  console.log("database connected")
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/url', urlRoute);
+app.use("/url", urlRoute);
+
+app.use("/", staticRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
