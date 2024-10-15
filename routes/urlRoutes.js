f96302f09477a6c13dd6afc6689ec83c094e90c5
AMAN7040 <amansingh79620@gmail.com>
2024-10-16 00:20:59 +0530

    Route to get the url result and analytics

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { generateNewShortUrl } = require("../controllers/url");
+const {
+  generateNewShortUrl,
+  getUrl,
+  getAnalytics,
+} = require("../controllers/url");
 const router = express.Router();
 
 router.post("/", generateNewShortUrl);
+
+router.get("/:id", getUrl);
+
+router.get("/analytics /:id", getAnalytics);
 
 module.exports = router;
