6cf680d059b9b18ed89c63c159c04f457f73310c
AMAN7040 <amansingh79620@gmail.com>
2024-10-15 20:59:50 +0530

    database setup and connected

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const urlRoute = require('./routes/urlRoutes')
+const {connectToDb} = require('./db')
+const app = express();
+const PORT = 3000;
+
+connectToDb("mongodb://127.0.0.1:27017/short-url")
+.then(()=> console.log('database connected'));
+
+app.use('/url', urlRoute);
+
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
