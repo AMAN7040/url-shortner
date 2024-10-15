d5cdaf38e611227ea0b810692662f54859fc3d6e
AMAN7040 <amansingh79620@gmail.com>
2024-10-16 00:21:47 +0530

    middleware used to parse json data

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = 3000;
 connectToDb("mongodb://127.0.0.1:27017/short-url")
 .then(()=> console.log('database connected'));
 
+app.use(express.json());
+
 app.use('/url', urlRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
